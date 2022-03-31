a9a7e298f3ec67437e78bca8c05df0a24eaa6973
Mátyás Budavári <budavariam@gmail.com>
2022-03-31 21:55:02 +0200

    Add annotations

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 - [Swagger in go](https://medium.com/@supun.muthutantrige/lets-go-everything-you-need-to-know-about-creating-a-restful-api-in-go-part-iv-52666c5221d4)
 - [goswagger usage](https://goswagger.io/use/spec.html)
+- [operation docs](https://goswagger.io/use/spec/operation.html)
+  - `swagger:operation [method] [path pattern] [?tag1 tag2 tag3] [operation id]`
+- [swagger:response](https://goswagger.io/use/spec/response.html)
--- src/main.go
+++ src/main.go
@@ -1,3 +1,31 @@
+// Package classification Account API.
+//
+// this is to show how to write RESTful APIs in golang.
+// that is to provide a detailed overview of the language specs
+//
+// Terms Of Service:
+//
+//     Schemes: http, https
+//     Host: localhost:8080
+//     Version: 1.0.0
+//     Contact: Supun Muthutantri<mydocs@example.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - api_key:
+//
+//     SecurityDefinitions:
+//     api_key:
+//          type: apiKey
+//          name: KEY
+//          in: header
+//
+// swagger:meta
 package main
 
 import (
@@ -15,11 +43,43 @@ import (
 //  "last_name":  "bolt",
 //  "user_name":  "james1234"
 //}
+// Account request model
 type Account struct {
-	ID        string `json:"id"`
+	// Id of the account
+	ID string `json:"id"`
+	// First Name of the account holder
 	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	UserName  string `json:"user_name"`
+	// Last Name of the account holder
+	LastName string `json:"last_name"`
+	// User Name of the account holder
+	UserName string `json:"user_name"`
+}
+
+// Account response payload
+// swagger:response accountRes
+type swaggAccountRes struct {
+	// in:body
+	Body Account
+}
+
+// Error Bad Request
+// swagger:response badReq
+type swaggReqBadRequest struct {
+	// in:body
+	Body struct {
+		// HTTP status code 400 -  Bad Request
+		Code int `json:"code"`
+	}
+}
+
+// Error Not Found
+// swagger:response notFoundReq
+type swaggReqNotFound struct {
+	// in:body
+	Body struct {
+		// HTTP status code 404 -  Not Found
+		Code int `json:"code"`
+	}
 }
 
 var accountMap map[string]Account
@@ -29,8 +89,58 @@ func init() {
 }
 func main() {
 	r := mux.NewRouter()
+	// swagger:operation POST /accounts/ accounts createAccount
+	// ---
+	// summary: Creates a new account.
+	// description: If account creation is success, account will be returned with Created (201).
+	// parameters:
+	// - name: account
+	//   description: account to add to the list of accounts
+	//   in: body
+	//   required: true
+	//   schema:
+	//     "$ref": "#/definitions/Account"
+	// responses:
+	//   "200":
+	//     "$ref": "#/responses/okResp"
+	//   "400":
+	//     "$ref": "#/responses/badReq"
 	r.HandleFunc("/accounts", createAccountHandler).Methods("POST")
+	// swagger:operation GET /accounts/{id} accounts getAccount
+	// ---
+	// summary: Return an Account provided by the id.
+	// description: If the account is found, account will be returned else Error Not Found (404) will be returned.
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: id of the account
+	//   type: string
+	//   required: true
+	// responses:
+	//   "200":
+	//     "$ref": "#/responses/accountRes"
+	//   "400":
+	//     "$ref": "#/responses/badReq"
+	//   "404":
+	//     "$ref": "#/responses/notFoundReq"
 	r.HandleFunc("/accounts/{id}", getAccountHandler).Methods("GET")
+	// swagger:operation DELETE /accounts/{id} accounts deleteAccount
+	// ---
+	// summary: Deletes requested account by account id.
+	// description: Depending on the account id, HTTP Status Not Found (404) or HTTP Status OK (200) may be returned.
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: account id
+	//   type: string
+	//   required: true
+	// responses:
+	//   "200":
+	//     "$ref": "#/responses/okResp"
+	//   "400":
+	//     "$ref": "#/responses/badReq"
+	//   "404":
+	//     "$ref": "#/responses/notFoundReq"
 	r.HandleFunc("/accounts/{id}", deleteAccountHandler).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
